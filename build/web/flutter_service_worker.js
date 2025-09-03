90404f5015b78592fa866676499dffe81b1b3af7
erablea <159782582+erablea@users.noreply.github.com>
2025-09-03 14:33:37 +0900

    Update web build with latest changes

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0f7673778bea1301fe665b366daaebff
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "5205fdd1e7a66980e089be380d4185aa",
+"flutter_bootstrap.js": "011c5be5bb7dddfdc1f8843579356a82",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
