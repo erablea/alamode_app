25014c653a7d1cfea0eeeb87b27a8fc6c069c254
erablea <erablea@users.noreply.github.com>
2026-05-19 03:40:17 +0000

    deploy: a25e56e7124f927217283b31743675f64ce83ed1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2786986138"
+    serviceWorkerVersion: "1845291652"
   }
 });
