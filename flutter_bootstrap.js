938d27d8aa733ca495e1404957351ecab4413da0
alamode-jpn <alamode-jpn@users.noreply.github.com>
2026-08-29 14:12:51 +0000

    deploy: 7681e0ffa55becdc503b4e2154b8f0fdf1cc15ce

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2728881482" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2238092664" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
